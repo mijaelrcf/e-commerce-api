2e39014e87c31e1cf1b145d13ed423202e7aa8a4
mijaelrcf <mijael.rcf@gmail.com>
2023-11-02 17:52:45 -0400

    Added data context and tables to the database

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using e_commerce_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
