36a9b625c0265815c3c1476b6f48f12902d98d82
mijaelrcf <mijael.rcf@gmail.com>
2023-11-02 18:06:49 -0400

    Addes controller for Customer

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using e_commerce_api.Models;
+using e_commerce_api.Services.CustomerService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetAll() 
+        { 
+            return await _customerService.GetAll();
+        }
+    }
+}
